b91fb5b9b8596278680a4ee1e769f1f6c578e5f8
Divyansh Agarwal <divyansh1199@gmail.com>
2021-06-14 00:50:27 +0530

    Setup basic flask structure

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+
+app.run(Debug=True)
